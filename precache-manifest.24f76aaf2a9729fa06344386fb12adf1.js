6799e17ff847e7c48446a69b5eda45f24baf8e6e
Birkhoff <juniorthx@gmail.com>
2020-03-02 15:04:31 -0500

    Updates

---
+++ precache-manifest.24f76aaf2a9729fa06344386fb12adf1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98eabbdb9cbfcca2248b2a6adeb013b6",
+    "url": "/react-drum-machine/index.html"
+  },
+  {
+    "revision": "9781758ec3803db7317c",
+    "url": "/react-drum-machine/static/css/2.7744f4b2.chunk.css"
+  },
+  {
+    "revision": "8b750699757c2bf36344",
+    "url": "/react-drum-machine/static/css/main.c927b3d8.chunk.css"
+  },
+  {
+    "revision": "9781758ec3803db7317c",
+    "url": "/react-drum-machine/static/js/2.4eeafc42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-drum-machine/static/js/2.4eeafc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b750699757c2bf36344",
+    "url": "/react-drum-machine/static/js/main.ca17bf0e.chunk.js"
+  },
+  {
+    "revision": "d688e06111db6889d38e",
+    "url": "/react-drum-machine/static/js/runtime-main.1f15f342.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-drum-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-drum-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-drum-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-drum-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-drum-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
